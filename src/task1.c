11d656cf4c26ede14ff2c9fbd5a111e05ff7eb2d
gcgc321 <gcgc321@gmail.com>
2026-01-05 12:31:14 +0100

    first commit

---
+++ src/task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 5;
+    float y = 3.140000;
+    char z[15] = "Hello World!";
+
+    printf("%d is a integer!\n", x);
+    printf("%f is a float!\n", y);
+    printf("%s is a char!\n", z);
+
+}
