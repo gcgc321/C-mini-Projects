d497d964fb9a1e3c8576821035010877488d918b
gcgc321 <gcgc321@gmail.com>
2026-01-09 13:19:33 +0100

    syscall

---
+++ src/task22.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main (void)
+{
+	int filedescriptor;
+
+	filedescriptor = open("testfile.txt", O_WRONLY | O_CREAT, S_IRWXU);
+
+	if (filedescriptor < 0)
+	{
+		printf("The open operation failed...");
+		return -1;
+	}
+	else 
+	{
+		printf("The open operation succeeded!");
+		return 0;
+	}
+	
+}
